42804c68819ec596c4ee9bc5c7f4fc12d11ec03b
amiyasingh <23ucc513@lnmiit.ac.in>
2026-06-15 17:40:04 +0530

    Bugs Fixed

--- src/routes/user.js
+++ src/routes/user.js
@@ -11,23 +11,33 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
       toUserId: loggedInUser._id,
       status: "interested",
     }).populate("fromUserId", ["firstName", "lastName"]);
+    const data = connectionRequest;
+    res.json({ data: connectionRequest });
   } catch (err) {
-    res.statusCode(400).send("ERROR: " + err.message);
+    res.status(400).send("ERROR: " + err.message);
   }
 });
 
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
+    const loggedInUser = req.user;
     const connectionRequests = await ConnectionRequest.find({
       $or: [
         { toUserId: loggedInUser._id, status: "accepted" },
         { fromUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId", ["firstName", "lastName"])
+    })
+      .populate("fromUserId", ["firstName", "lastName"])
+      .populate("toUserId", ["firstName", "lastName"]);
 
-    const data = connectionRequests.map((row) => row.fromUserId);
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
 
-    res.json({data});
+    res.json({ data });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
