d5153a083aafc3abc3205cb96b1d96c8e83e857a
amiyasingh <23ucc513@lnmiit.ac.in>
2026-06-15 17:04:59 +0530

    POST /user/connection API created

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,19 +3,34 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 
-
 // get all the pending connection request for the logged in user
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
-    try{
-        const loggedInUser = req.user;
-        const connectionRequest = await ConnectionRequest.find({
-            toUserId: loggedInUser._id,
-            status: "interested",
-        }).populate("fromUserId", ["firstname", "lastName"]);
-    }catch (err) {
-        res.statusCode(400).send("ERROR: "+err.message);
-    }
+  try {
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
+  } catch (err) {
+    res.statusCode(400).send("ERROR: " + err.message);
+  }
+});
 
-})
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", ["firstName", "lastName"])
+
+    const data = connectionRequests.map((row) => row.fromUserId);
+
+    res.json({data});
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
 
 module.exports = userRouter;
